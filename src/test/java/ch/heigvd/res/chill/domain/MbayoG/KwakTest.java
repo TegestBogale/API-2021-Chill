863b2edb6a5d6bbea3aa49d5d869e9f3d7a2b3b5
Ancalagon <gmbayo11@gmail.com>
2021-09-23 12:06:37 +0200

    Add Kwak with test

---
+++ src/test/java/ch/heigvd/res/chill/domain/MbayoG/KwakTest.java
@@ -0,0 +1,31 @@
+package ch.heigvd.res.chill.domain.MbayoG;
+
+import ch.heigvd.res.chill.domain.Bartender;
+import ch.heigvd.res.chill.protocol.OrderRequest;
+import ch.heigvd.res.chill.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class KwakTest {
+
+  @Test
+  void thePriceAndNameForKwakShouldBeCorrect() {
+    Kwak beer = new Kwak();
+    assertEquals(beer.getName(), Kwak.NAME);
+    assertEquals(beer.getPrice(), Kwak.PRICE);
+  }
+
+  @Test
+  void aBartenderShouldAcceptAnOrderForKwak() {
+    Bartender jane = new Bartender();
+    String productName = "ch.heigvd.res.chill.domain.MbayoG.Kwak";
+    OrderRequest request = new OrderRequest(3, productName);
+    OrderResponse response = jane.order(request);
+    BigDecimal expectedTotalPrice = Kwak.PRICE.multiply(new BigDecimal(3));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
