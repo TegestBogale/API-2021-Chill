592a813e0e3b41dfaab99a202426615f3db4a9ce
boogieman <anthony.coke@heig-vd.ch>
2021-09-27 15:43:49 +0200

    refactor: change forgotten PunkIPA

--- src/test/java/ch/heigvd/res/chill/domain/b0ogieMan/TenebreuseTest.java
+++ src/test/java/ch/heigvd/res/chill/domain/b0ogieMan/TenebreuseTest.java
@@ -1,7 +1,7 @@
 package ch.heigvd.res.chill.domain.b0ogieMan;
 
 import ch.heigvd.res.chill.domain.Bartender;
-import ch.heigvd.res.chill.domain.wasadigi.PunkIPA;
+import ch.heigvd.res.chill.domain.b0ogieMan.Tenebreuse;
 import ch.heigvd.res.chill.protocol.OrderRequest;
 import ch.heigvd.res.chill.protocol.OrderResponse;
 import org.junit.jupiter.api.Test;
@@ -13,16 +13,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class TenebreuseTest {
 
   @Test
-  void thePriceAndNameForPunkIPAShouldBeCorrect() {
+  void thePriceAndNameForTenebreuseShouldBeCorrect() {
     Tenebreuse beer = new Tenebreuse();
     assertEquals(beer.getName(), Tenebreuse.NAME);
     assertEquals(beer.getPrice(), Tenebreuse.PRICE);
   }
 
   @Test
-  void aBartenderShouldAcceptAnOrderForPunkIPA() {
+  void aBartenderShouldAcceptAnOrderForTenebreuse() {
     Bartender jane = new Bartender();
-    String productName = "ch.heigvd.res.chill.domain.wasadigi.PunkIPA";
+    String productName = "ch.heigvd.res.chill.domain.b0ogieMan.Tenebreuse";
     OrderRequest request = new OrderRequest(3, productName);
     OrderResponse response = jane.order(request);
     BigDecimal expectedTotalPrice = Tenebreuse.PRICE.multiply(new BigDecimal(3));
